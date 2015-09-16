c5e6f3f2638318efa9c911dd6364b5a2107c227f
slk000 <sunlingkun@hotmail.com>
2015-09-16 22:03:18 +0800

    Initial Commit

---
+++ com.slk.FreshmanGuider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  com.slk.FreshmanGuider
+//
+//  Created by Takanashirin on 15/9/16.
+//  Copyright © 2015年 孙凌昆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ com.slk.FreshmanGuider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  com.slk.FreshmanGuider
+//
+//  Created by Takanashirin on 15/9/16.
+//  Copyright © 2015年 孙凌昆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
